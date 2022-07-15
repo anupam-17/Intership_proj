cdc56504f0a593fa51e7751584eeef11f5247f28
anupam-17 <64515650+anupam-17@users.noreply.github.com>
2022-07-15 17:38:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+// alert("gjgjgyug");
+document.getElementsByClassName("input-space").style.display="none";
+//function searchButton()
+//
+//   document.querySelector("input").style="type:search";
+//
